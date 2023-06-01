ac1b44e6742f3d06eb518ba752d0e95bac1fd08a
fanxin li <lexxshinjj@gmail.com>
2023-06-01 19:17:55 +0800

    final

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # root to: "tasks#index"
+  # get "tasks/new", to: "tasks#new", as: :new
+  # post "tasks", to: "tasks#create"
+  # get "/:id", to: "tasks#show", as: :task
+  # get "/:id/edit", to: "tasks#edit", as: :edit
+  # patch "/:id", to: "tasks#update"
+  # delete "/:id", to: "tasks#destroy"
+  resources :tasks
 end
